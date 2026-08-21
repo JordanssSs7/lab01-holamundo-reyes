02a2654f3494cff1987ac2c4bb829da96f7dae4a
Jordan Reyes <arturosaravia32@gmail.com>
2026-08-21 17:09:08 -0500

    agregué boton 'presióname'

--- app/src/main/java/com/reyes/laboratorio01/MainActivity.kt
+++ app/src/main/java/com/reyes/laboratorio01/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -50,8 +51,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             text = "Carrera: Diseño y desarrollo de software"
         )
 
+        Button(onClick = { }) {
+            Text("Presióname")
+        }
+
     }
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
